7800d5fa7e9418ff64c563eade3c2c7b0d340427
Joey Mckenzie <joey32793@gmail.com>
2018-04-24 22:26:25 -0700

    Updating runtimes

--- BudgetPeeker/Program.cs
+++ BudgetPeeker/Program.cs
@@ -12,7 +12,6 @@ namespace BudgetPeeker
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("https://budgetpeeker.azurewebsites.net")
                 .UseStartup<Startup>();
     }
 }
